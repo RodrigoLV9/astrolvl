de2607532dbe19de066be08ee477c7fb51f3588e
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-05-27 18:46:26 -0300

    fix, style(FaqsCard): Fix and adjust some structures and styles

--- src/components/Faqs/FaqsCard.jsx
+++ src/components/Faqs/FaqsCard.jsx
@@ -9,12 +9,9 @@ export const FaqsCard=()=>{
     }
     return(
         <div className="faqsCard">
-            <div className="faqsCard__header">
+            <div className="faqsCard__header" onClick={handleSelect}>
                 <h3>What is the purpose of this project?</h3>
-                <button onClick={handleSelect} className='faqsCard__header-button'>
-                    <SelectIcon className={select ? "faqsCard__header-icon" : "faqsCard__header-icon--select"}/>
-                </button>
-                
+                <SelectIcon className={select ? "faqsCard__header-icon" : "faqsCard__header-icon--select"}/>
             </div>
             <div className={select ? 'faqsCard__content-select' : 'faqsCard__content'}>
                 <p>This project is designed to provide a comprehensive overview of the features and functionalities of our application.</p>
