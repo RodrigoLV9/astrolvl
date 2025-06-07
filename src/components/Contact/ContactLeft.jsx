a95de4f446d5c3b95c4a78099ef7e3774e6b20ec
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-06-07 16:58:15 -0300

    fix(ContactLeft): Fix validation config

--- src/components/Contact/ContactLeft.jsx
+++ src/components/Contact/ContactLeft.jsx
@@ -2,21 +2,22 @@ import { BsFillSendFill as SendIcon } from "react-icons/bs";
 import '../../styles/contactLeft.css'
 import {useState, useEffect, useRef} from 'react'
 import { useForm } from "react-hook-form"
-import {SchemaForm} from '../../validations/SchemaForm.js'
+import {SchemaContact} from '../../validations/SchemaContact.js'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { FaCheck as CheckIcon } from "react-icons/fa";
+import { MdErrorOutline as ErrorIcon } from "react-icons/md";
 export const ContactLeft=()=>{
     const [error,setError]=useState(undefined)
     const [isSubmitted, setIsSubmitted]=useState(false)
     const formRef=useRef(null)
     const { register, handleSubmit, formState: { errors } } = useForm({
-        resolver: zodResolver(SchemaForm)
+        resolver: zodResolver(SchemaContact())
     });
     useEffect(()=>{
-        console.log('Errors: ',errors)
         setError({ 
-            name:errors.name.message,
-            email:errors.email.message,
-            message:errors.message.message
+            name:errors.name?.message,
+            email:errors.email?.message,
+            message:errors.message?.message
         })
         if(errors.name || errors.email || errors.message){
             setIsSubmitted(false)
@@ -33,29 +34,37 @@ export const ContactLeft=()=>{
                 <div className="form__group">
                     <label htmlFor="name">Nombre</label>
                     <input type="text" id="name" name="name" required {...register('name')}/>
-                    
+                    {errors.name && (
+                        <span className='errorMessage'>
+                          <ErrorIcon className="errorMessage__icon"/>
+                          <p>{error?.name}</p>
+                        </span>
+                    )}
                 </div>
                 <div className="form__group">
                     <label htmlFor="email">Email</label>
                     <input type="email" id="email" name="email" required {...register('email')}/>
+                    {errors.email && (
+                        <span className='errorMessage'>
+                          <ErrorIcon className="errorMessage__icon"/>
+                          <p>{error?.email}</p>
+                        </span>
+                    )}
                 </div>
                 <div className="form__group">
                     <label htmlFor="message">Mensaje</label>
                     <textarea id="message" name="message" required {...register('message')} ></textarea>
+                    {errors.message && (
+                        <span className='errorMessage'>
+                          <ErrorIcon className="errorMessage__icon"/>
+                          <p>{error?.message}</p>
+                        </span>
+                    )}
                 </div>
                 <button className='formButton' type="submit" id='button'>
                     <p>Send</p>
                     <SendIcon className='formButton__icon'/>
                 </button>
-                {/* {
-                    error?.name || error?.email || error?.message ? 
-                    <p>{error?.name || error?.email || error?.message}</p>
-                    :
-                    <p>Correct</p>
-                } */}
-                {errors.name && (
-                    <p className="error-message">{error?.name}</p>
-                )}
                 <input type="hidden" name="_captcha" value="false"/>
             </form>
         </div> 
---
+++ src/validations/SchemaContact.js
@@ -0,0 +1,6 @@
+import { z } from "zod";
+export const SchemaContact = () => z.object({
+    name: z.string().min(5,{message:'El nombre debe tener al menos 5 letras'}).max(12,{message:'El nombre debe tener como máximo 12 letras'}),
+    email: z.string().email({message:'El correo electrónico es incorrecto'}),
+    message: z.string().min(15,{messsage:'El mensaje debe tener al menos 15 caracteres'})
+});
