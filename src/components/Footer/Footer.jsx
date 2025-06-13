b51460a54a56117f91225d692201dcfa05b7ada3
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-06-12 23:16:57 -0300

    style(Footer): Add responsive design for Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,33 +10,36 @@ export const Footer=()=>{
                     <LogoMain/>
                     <p>Agencia de diseño web. Brindamos soluciones personalizadas para impulsar tu presencia online.</p>
                 </div>
-                <div className="footerTop__contact">
-                    <h2>Contact</h2>
-                    <ul>
-                        {
-                            mainSocialNetworks.map((item,index)=>(
-                                <li key={index}>
-                                    {item.icon2}
-                                    <p>{item.info}</p>
-                                </li>
-                            ))
-                        }
-                    </ul>
+                <div className='footerTop__side'>
+                    <div className="footerTop__contact">
+                        <h2>Contact</h2>
+                        <ul>
+                            {
+                                mainSocialNetworks.map((item,index)=>(
+                                    <li key={index}>
+                                        {item.icon2}
+                                        <p>{item.info}</p>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                    <div className='footerTop__follow'>
+                        <h2>Follow us</h2>
+                        <ul>
+                            {
+                                socialNetworks.map((social,index)=>(
+                                    <li key={index}>
+                                        <a href={social.link} target="_blank">
+                                            {social.icon}
+                                        </a>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
                 </div>
-                <div className='footerTop__follow'>
-                    <h2>Follow us</h2>
-                    <ul>
-                        {
-                            socialNetworks.map((social,index)=>(
-                                <li key={index}>
-                                    <a href={social.link} target="_blank">
-                                        {social.icon}
-                                    </a>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </div>
+                
             </section>
             <p className='footerBottom'>© 2025 Astro LvL | Agencia de desarrollo web en Argentina</p>
         </footer>
