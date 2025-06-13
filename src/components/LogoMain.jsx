26ec5f40c75a751c1391d8b3696efed64e00f4a6
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-06-13 00:07:39 -0300

    fix(images): Fix src of images in public folder

--- src/components/LogoMain.jsx
+++ src/components/LogoMain.jsx
@@ -2,7 +2,7 @@ import '../styles/logoMain.css'
 export const LogoMain = () => {
   return (
     <div className="header__logo">
-        <img src="/public/images/astrolvl-logo.png" alt="logo_main" class="header__logo-img" width="50" height="50"/>
+        <img src="/images/astrolvl-logo.png" alt="logo_main" className="header__logo-img" width="50" height="50"/>
         <p>ASTRO LvL</p>
     </div>
   )
